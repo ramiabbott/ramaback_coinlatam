47cca1e9772bb2958fdb0f2c087a31ebd59f5362
Ramiro <ramiroignacio_14@hotmail.com>
2024-06-10 13:27:49 -0300

    rama 05

--- src/database/mongo/connection.ts
+++ src/database/mongo/connection.ts
@@ -1,5 +1,4 @@
 import mongoose from "mongoose";
-import colors from 'colors';
 // import dotenv from 'dotenv';
 //dotenv.config();
 import { envs } from "../../config/plugin/env-var";
@@ -36,13 +35,13 @@ const mongoDB = async (callback: () => void, reset: boolean) => {
       if (reset) {
         await mongoose.connection.dropDatabase();
       }
-      console.log(colors.green.bold.italic("Connected to MongoDB"), reset ? colors.red.italic("| Initial dropped") : "");
+      console.log("Connected to MongoDB", reset ? "| Initial dropped" : "");
     })
     .then(() => {
       callback();
     })
     .catch((error) => {
-      console.log(colors.bgRed.italic("Failed to connect to MongoDB"))
+      console.log("Failed to connect to MongoDB")
       console.error("Failed to connect to MongoDB:", error);
     });
 };
